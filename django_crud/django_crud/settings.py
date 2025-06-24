795940b036604f4945d4a7ac3eec0ea914ed907c
Ashish Auti <ashishauti123@gmail.com>
2025-06-24 12:30:36 +0530

    chore : configure simple apps for specific feature

--- django_crud/django_crud/settings.py
+++ django_crud/django_crud/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
+    "calender_app"
 ]
 
 MIDDLEWARE = [
--- django_crud/django_crud/urls.py
+++ django_crud/django_crud/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", include('app.urls')),
+    path("calender/", include('calender_app.urls'))
+
 ]
