f2cf559e2226cb13337351f449462a19d1b417a7
Ashish Auti <ashishauti123@gmail.com>
2025-06-24 18:15:38 +0530

    refactor : black formater

--- django_crud/app/urls.py
+++ django_crud/app/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import EventViewSet
+
 urlpatterns = [
-    path('api/', EventViewSet.as_view({'post':'create', 'get':'list'}), name="apis"),
-    path('api/<int:pk>', EventViewSet.as_view({"delete":'destroy', 'put':'update'}))
+    path("api/", EventViewSet.as_view({"post": "create", "get": "list"}), name="apis"),
+    path(
+        "api/<int:pk>",
+        EventViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
